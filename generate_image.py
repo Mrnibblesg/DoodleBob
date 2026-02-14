37368e35a7902f6a0d129294b1b33a56956dcffc
Parker Gutierrez <parkerg444@gmail.com>
2026-02-13 17:28:19 -0800

    Show an image in color and in grayscale using cv2 and matplotlib

--- generate_image.py
+++ generate_image.py
@@ -1,17 +1,23 @@
 import cv2 as cv
 import sys
+import matplotlib.pyplot as plt
+
 if (len(sys.argv) < 2):
     raise ValueError(f"Expected at least one command line argument, got {len(sys.argv) - 1}")
 
 
 def main():
-    print(sys.argv)
     # Take input image, convert to black and white with thresholding
-    try:
+    fig, ax = plt.subplots(1, 2)
 
-        img = cv.imread()
-    except:
+    img = cv.imread(sys.argv[1], cv.IMREAD_GRAYSCALE)
 
+    img_color = cv.imread(sys.argv[1])
+    img_color = cv.cvtColor(img_color, cv.COLOR_BGR2RGB)
+
+    ax[0].imshow(img, cmap='gray')
+    ax[1].imshow(img_color)
+    plt.show()
 
 
 if __name__ == "__main__":
