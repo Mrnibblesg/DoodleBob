908dad6302d6a548f4fb5bb4d5f51fa857aaf348
Parker Gutierrez <parkerg444@gmail.com>
2026-02-13 17:50:06 -0800

    Add Canny edge detection

--- generate_image.py
+++ generate_image.py
@@ -8,15 +8,18 @@ if (len(sys.argv) < 2):
 
 def main():
     # Take input image, convert to black and white with thresholding
-    fig, ax = plt.subplots(1, 2)
+    fig, ax = plt.subplots(1, 3)
 
     img = cv.imread(sys.argv[1], cv.IMREAD_GRAYSCALE)
 
     img_color = cv.imread(sys.argv[1])
     img_color = cv.cvtColor(img_color, cv.COLOR_BGR2RGB)
-
+    lower = 100
+    upper = 200
+    img_edges = cv.Canny(img, lower, upper)
     ax[0].imshow(img, cmap='gray')
     ax[1].imshow(img_color)
+    ax[2].imshow(img_edges)
     plt.show()
 
 
