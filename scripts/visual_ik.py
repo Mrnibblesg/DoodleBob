56e36ebd663e66683b9076178dc04bdaea86719e
Parker Gutierrez <parkerg444@gmail.com>
2026-06-08 00:42:13 -0700

    Fix orientation mode in visual_ik debug script.

--- scripts/visual_ik.py
+++ scripts/visual_ik.py
@@ -3,10 +3,11 @@ import ikpy.utils.plot as plot_utils
 import numpy as np
 import matplotlib.pyplot as plt
 
+# To view what it's like when the arm can't reach a point, set all 0.25 to 0.3
 c1 = [0.1, 0.1, 0.15]
-c2 = [0.1, 0.6, 0.15]
-c3 = [0.6, 0.6, 0.15]
-c4 = [0.6, 0.1, 0.15]
+c2 = [0.1, 0.25, 0.15]
+c3 = [0.25, 0.25, 0.15]
+c4 = [0.25, 0.1, 0.15]
 targets = [c1, c2, c3, c4, c1]
 
 start_conf = [0.0, -1.57, 0.0, -1.57, 0.0, 0.0]
@@ -27,15 +28,28 @@ def main():
         start = targets[idx]
         end = targets[idx+1]
         all_targets.extend(interpolate_trajectory(start, end))
-    #ax.plot([0,1], [0,1], 'red', zs=[0,-1])
-    
+    ax.plot([0,1], [0,1], 'red', zs=[0,-1])
+
+
+    # To view the arm's configuration when it targets this point
+    debug_point = c3
+
     reached = []
+    zero_config = [0] * len(chain.links)
+    prev_angles = zero_config
+
     # Generate all actual points that the robot arm goes to
     for point in all_targets:
-        reached.append(arm_point_from_coord(chain, point))
+        end_point, prev_angles = arm_config_from_coord(chain, point, prev_angles)
+
+        if debug_point is not None and point[0] == debug_point[0] and point[1] == debug_point[1] and point[2] == debug_point[2]:
+            chain.plot(prev_angles, ax, target=point)
+
+        reached.append(end_point)
     # chain.plot(chain.inverse_kinematics(targets[0]), ax, target=targets[0])
     # visualize_points(all_targets, ax, "red")
     visualize_points(reached, ax, "blue")
+
     plt.show()
 
 # Return a list of points resulting from an interpolated cartesian trajectory
@@ -54,7 +68,6 @@ def interpolate_trajectory(start, end, interval=0.025):
         points.append(next.copy())
     
     points.append(end)
-    print(points)
     return points
 
 def make_target_frame(x, y, z):
@@ -67,9 +80,15 @@ def make_target_frame(x, y, z):
     return frame
 
 # Return the FK point after trying to make the chain reach the coordinate
-def arm_point_from_coord(chain, coordinate):
+def arm_config_from_coord(chain, coordinate, prev_angles):
     target_frame = make_target_frame(*coordinate)
-    return chain.forward_kinematics(chain.inverse_kinematics_frame(target_frame))[:3, 3]
+    angles = chain.inverse_kinematics_frame(
+            target_frame,
+            initial_position=prev_angles,
+            orientation_mode="Z"
+    )
+
+    return chain.forward_kinematics(angles)[:3, 3], angles
 
 
 # Draw a line between each point.
